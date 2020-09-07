992cf3b956353c2d2a693c5eac16466e234a842c
kmin-jeong <desk535353@gmail.com>
2020-09-08 01:16:02 +0900

    feat: solve 2750 & 7568

---
+++ algorithims/0907/2750.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+nums = []
+
+for _ in range(N) : 
+    nums.append(int(input()))
+
+for i in range(len(nums)) : 
+    for j in range(len(nums)) : 
+        if nums[i] < nums[j] : 
+            nums[i], nums[j] = nums[j], nums[i]
+            
+for n in nums : 
+    print(n)
